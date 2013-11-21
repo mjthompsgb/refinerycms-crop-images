377364d233180335f8a4be551fee91e248ca5e55
Matt Thompson <mthompson@wspackaging.com>
2013-11-20 22:03:02 -0600

    removed admin tab

--- lib/refinery/crop_images/engine.rb
+++ lib/refinery/crop_images/engine.rb
@@ -6,17 +6,7 @@ module Refinery
 
       engine_name :refinery_crop_images
 
-      initializer "register refinerycms_crop_images plugin" do
-        Refinery::Plugin.register do |plugin|
-          plugin.name = "crop_images"
-          plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.crop_images_admin_crop_images_path }
-          plugin.pathname = root
-          plugin.activity = {
-            :class_name => :'refinery/crop_images/crop_image'
-          }
-          
-        end
-      end
+      
 
       config.after_initialize do
         Refinery.register_extension(Refinery::CropImages)
