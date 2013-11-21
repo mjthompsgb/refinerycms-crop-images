4b88de8bbbfa83e71980a0a77ee821364a94839c
Matt Thompson <mthompson@wspackaging.com>
2013-11-20 22:19:47 -0600

    removed admin tab

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 (Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
   I18n.locale = lang
 
-  
+  if defined?(Refinery::User)
+    Refinery::User.all.each do |user|
+      if user.plugins.where(:name => 'refinerycms-crop_images').blank?
+        user.plugins.create(:name => 'refinerycms-crop_images',
+                            :position => (user.plugins.maximum(:position) || -1) +1)
+      end
+    end
+  end
 
-  
+
 end
